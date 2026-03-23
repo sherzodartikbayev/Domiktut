4cb91d5d9307cf4e09ea82235ecc12eb6ab6c3f9
Sherzod Artikbayev <info@web-pro.uz>
2026-03-23 12:38:29 +0500

    chore: configure server

---
+++ backend/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config()
+
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'public')))
+
+const PORT = process.env.PORT
+
+app.listen(PORT, console.log(`Server is running on: ${PORT}`))
