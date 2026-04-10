00793873e1756b00b4c17cea467072d77aa006ab
Sherzod Artikbayev <info@web-pro.uz>
2026-04-11 00:40:25 +0500

    feat: Home page, added carousel cards

---
+++ frontend/src/constants/index.ts
@@ -0,0 +1,32 @@
+export const homeCardCarousel = [
+	{
+		id: 1,
+		src: '/images/carousel/photo 1.jpg',
+		label: 'С бассейном'
+	},
+	{
+		id: 2,
+		src: '/images/carousel/photo 2.jpg',
+		label: "Семейные и уютные"
+	},
+	{
+		id: 3,
+		src: '/images/carousel/photo 3.jpg',
+		label: "Хиты продаж"
+	},
+	{
+		id: 1,
+		src: '/images/carousel/photo 1.jpg',
+		label: 'С бассейном'
+	},
+	{
+		id: 2,
+		src: '/images/carousel/photo 2.jpg',
+		label: "Семейные и уютные"
+	},
+	{
+		id: 3,
+		src: '/images/carousel/photo 3.jpg',
+		label: "Хиты продаж"
+	}
+]
--- frontend/src/pages/user/home.tsx
+++ frontend/src/pages/user/home.tsx
@@ -1,7 +1,18 @@
+import { Card, CardContent } from '../../components/ui/card'
+import {
+	Carousel,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from '../../components/ui/carousel'
+import { homeCardCarousel } from '../../constants'
+import Autoplay from 'embla-carousel-autoplay'
+
 const HomePage = () => {
 	return (
-		<section className='pt-36 home-bg'>
-			<div className='container h-screen'>
+		<section className=' home-bg'>
+			<div className='container h-237 flex-between'>
 				<div className='max-w-2xl'>
 					<h1 className='font-raleway font-extrabold text-6xl leading-[140%] text-white'>
 						Аренда коттеджей и домов в Казани
@@ -10,6 +21,49 @@ const HomePage = () => {
 					<p className='font-semibold text-2xl leading-[150%] text-white mt-7 mb-24'>
 						Найдите идеальный вариант сами или предоставьте это нам
 					</p>
+
+					<Carousel
+						opts={{
+							align: 'start',
+							loop: true,
+						}}
+						plugins={[
+							Autoplay({
+								delay: 2000,
+							}),
+						]}
+						className='w-full max-w-48 sm:max-w-xs md:max-w-150'
+					>
+						<CarouselContent className='flex gap-1'>
+							{homeCardCarousel.map(item => (
+								<CarouselItem key={item.id} className='basis-1/2 lg:basis-1/3'>
+									<Card className='bg-transparent place-items-center'>
+										<CardContent className='w-50 flex flex-col items-center justify-center'>
+											<img
+												src={item.src}
+												alt={item.label}
+												className='w-50 h-24 object-cover rounded-2xl mb-3.5'
+											/>
+
+											<span className='font-semibold text-[18px] text-center text-white'>
+												{item.label}
+											</span>
+										</CardContent>
+									</Card>
+								</CarouselItem>
+							))}
+						</CarouselContent>
+						<CarouselPrevious />
+						<CarouselNext />
+					</Carousel>
+				</div>
+
+				<div className=''>
+					<div className='w-90 h-124.25 rounded-[20px] bg-white py-10 px-10'>
+						<h3 className='text-xl font-semibold text-center'>
+							Заявка на подбор
+						</h3>
+					</div>
 				</div>
 			</div>
 		</section>
